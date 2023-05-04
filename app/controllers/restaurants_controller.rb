486784b86e9eccd5da2b64c432698b88d6f4e0f2
YeinerCQ <ycarvajalq@gmail.com>
2023-05-04 11:46:19 -0500

    add update and destroy methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,6 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all.ordered
   end
@@ -7,17 +9,42 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path, notice: "Restaurant successfuly created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurants_path, notice: "Restaurant successfuly updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
-  def detroy
+  def destroy
+    if @restaurant.destroy
+      redirect_to restaurants_path, notice: "Restaurant successfuly destroyed."
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :city, :address)
   end
 end
