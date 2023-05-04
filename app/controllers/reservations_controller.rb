1459d54c6f4bb8b487e34aaf451f2cdf3eaa10d7
YeinerCQ <ycarvajalq@gmail.com>
2023-05-04 15:34:50 -0500

    add reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,16 +1,48 @@
 class ReservationsController < ApplicationController
+  before_action :set_restaurant
+  before_action :set_reservation, only: [:edit, :update, :destroy]
+
   def new
+    @reservation = @restaurant.reservations.build
   end
 
   def create
+    @reservation = @restaurant.reservations.build(reservation_params)
+    if @reservation.save
+      redirect_to restaurant_path(@restaurant), notice: "Reservation successfuly created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @reservation.update(reservation_params)
+      redirect_to restaurant_path(@restaurant), notice: "Reservation successfuly edited."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    if @reservation.destroy
+      redirect_to restaurant_path(@restaurant), notice: "Reservation successfuly destroyed."
+    end
+  end
+
+  private
+
+  def set_reservation
+    @reservation = @restaurant.reservations.find(params[:id])
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:booking_date)
   end
 end
