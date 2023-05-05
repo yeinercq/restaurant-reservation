b587726986da81496eb5faad9a2f3e90224deaac
YeinerCQ <ycarvajalq@gmail.com>
2023-05-04 22:35:44 -0500

    add update and destroy methods to restaurants api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,27 +3,49 @@ class ApiController < ActionController::API
     render json: {api: "OK"}, status: :ok
   end
 
+  # Restaurants
+
+  # index
   def restaurants
     @restaurants = Restaurant.all.ordered
     render json: @restaurants, status: :ok
   end
 
+  # show
   def restaurant
     @restaurant = Restaurant.find(params[:id])
     render json: @restaurant, status: :ok
   end
 
+  # Create
+  def create_restaurant
+    @restaurant = Restaurant.create!(restaurant_params)
+    render json: @restaurant, status: :created
+  end
+
+  # Destroy
+  def destroy_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    # status: :ok
+  end
+
+  # Update
+  def update_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update!(restaurant_params)
+  end
+
+  # Reservations
+
+  # Index
   def reservations
     @restaurant = Restaurant.find(params[:id])
     @reservations = @restaurant.reservations.ordered
     render json: @reservations, status: :ok
   end
 
-  def create_restaurant
-    @restaurant = Restaurant.create!(restaurant_params)
-    render json: @restaurant, status: :created
-  end
-
+  # Create
   def create_reservation
     @restaurant = Restaurant.find(params[:id])
     @reservation = @restaurant.reservations.create!(reservation_params)
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,15 @@ Rails.application.routes.draw do
   root "restaurants#index"
 
   get '/api/health', to: "api#health"
+
+  # API to Restaurants
   get '/api/restaurants', to: "api#restaurants"
-  post '/api/restaurants/', to: "api#create_restaurant"
   get '/api/restaurants/:id/', to: "api#restaurant"
+  post '/api/restaurants/', to: "api#create_restaurant"
+  delete '/api/restaurants/:id', to: "api#destroy_restaurant"
+  patch '/api/restaurants/:id', to: "api#update_restaurant"
+
+  # API to reservations listing  
   get '/api/restaurants/:id/reservations/', to: "api#reservations"
   post '/api/restaurants/:id/reservations/', to: "api#create_reservation"
 
